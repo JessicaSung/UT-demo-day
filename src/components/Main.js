d67a5be5e3ca3995df4ead54ab48030a0cbb4b0b
JessicaSung <jessica.sung@utexas.edu>
2017-02-21 17:34:19 -0600

    initial file setup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
 
 export default class Main extends Component {
 	render() {		
 		return (
-			<div>
-				<h3>Thank you for attending!</h3>
-			</div>
+			<MuiThemeProvider>
+		   		<h3>Thank you for attending!</h3>
+		  	</MuiThemeProvider>
 		);
 	}
 }
